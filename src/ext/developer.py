fa45fbc4ba9a89ec76d6b1beca5fc0908ce35905
root <root@rrrrd97mailru.fvds.ru>
2025-02-03 07:59:08 +0100

    Оптимизация - developer.py

--- src/ext/developer.py
+++ src/ext/developer.py
@@ -1,10 +1,6 @@
 import disnake
 from disnake.ext import commands
-from datetime import datetime
-import re
-
 from src.emoire import emoire
-from src.core.classes import PickOne
 
 class Developer(commands.Cog):
     def __init__(self, bot):
@@ -12,32 +8,33 @@ class Developer(commands.Cog):
 
     @commands.slash_command(name='clear')
     @commands.has_permissions(administrator=True)
-    async def clear(
-            self, 
-            interaction: disnake.GuildCommandInteraction, 
-            count: int, 
-        ):
-        if interaction.author.id == 627925818429145119:
-            await interaction.channel.purge(limit=count + 1)
-            await interaction.send("успешно", delete_after=5)
-            
+    async def clear(self, interaction: disnake.GuildCommandInteraction, count: int):
+        """Удаляет указанное количество сообщений в канале."""
+        if interaction.author.id != 627925818429145119:
+            return await interaction.send("У вас нет прав для использования этой команды.", ephemeral=True)
+
+        await interaction.channel.purge(limit=count + 1)  # Удаляем указанное количество сообщений + само сообщение с командой
+        await interaction.send("Сообщения успешно удалены.", delete_after=5)
+
     @commands.slash_command(name='reboot')
     @commands.has_permissions(administrator=True)
-    async def reboot(self, interaction: disnake.GuildCommandInteraction, extension: str = commands.Param(choices=['admin','other','developer', 'listeners', 'fun'])):
+    async def reboot(self, interaction: disnake.GuildCommandInteraction, extension: str = commands.Param(choices=['admin', 'other', 'developer', 'listeners'])):
+        """Перезагружает указанное расширение."""
         try:
             self.bot.reload_extension(f'src.ext.{extension}')
-            await interaction.send(f'Extension: `{extension}` успешно перезагружен.')
+            await interaction.send(f'Расширение `{extension}` успешно перезагружено.')
         except Exception as e:
-            await interaction.send(f'Ошибка при перезагрузке кода: {e}')
-    
+            await interaction.send(f'Ошибка при перезагрузке расширения: {e}')
+
     @commands.slash_command(name='loaded')
     @commands.has_permissions(administrator=True)
     async def loaded(self, interaction: disnake.GuildCommandInteraction, extension: str):
+        """Загружает указанное расширение."""
         try:
             self.bot.load_extension(f'src.ext.{extension}')
-            await interaction.send(f'Extension: `{extension}` успешно загружен.')
+            await interaction.send(f'Расширение `{extension}` успешно загружено.')
         except Exception as e:
-            await interaction.send(f'Ошибка при перезагрузке кода: {e}')
+            await interaction.send(f'Ошибка при загрузке расширения: {e}')
 
 def setup(bot):
     bot.add_cog(Developer(bot))
